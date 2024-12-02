fca5b99a6723e2a3df413f0bdd38bb811a6a5d73
Jonathan Good <github@bungogood.com>
2024-12-02 17:27:22 +0000

    2024 1st & 2nd

--- app/Solver.hs
+++ app/Solver.hs
@@ -4,6 +4,8 @@ module Solver (Solver (..), getSolver, runSolver) where
 
 import Lib
 -- REPLACE_IMPORTS_START
+import qualified Y2024.Day01
+import qualified Y2024.Day02
 -- REPLACE_IMPORTS_END
 
 data Solver = forall a. (Solvable a) => Solver a
@@ -16,5 +18,7 @@ getSolver :: Int -> Int -> Solver
 getSolver year day =
   case (year, day) of
 -- REPLACE_CASES_START
+    (2024, 1) -> Solver Y2024.Day01.Solution
+    (2024, 2) -> Solver Y2024.Day02.Solution
 -- REPLACE_CASES_END
     _ -> error "Invalid year or day"
